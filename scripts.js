75989924ed21738a31b113ff1c562946cbc08de4
Alejandro Hernández Polo <jhalejandro@uninorte.edu.co>
2023-03-18 22:05:28 -0500

    Sway sadly style

---
+++ scripts.js
@@ -0,0 +1,41 @@
+/* Scroll Reveal */
+window.addEventListener('scroll', function() {
+  var header = document.querySelector('header');
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// One
+// ScrollReveal().reveal('.mini-header', {
+//   duration: 2000,
+//   distance: '150%',
+//   origin: 'right',
+//   opacity: null
+// });
+
+ScrollReveal().reveal('.title', {
+  duration: 2000,
+  distance: '150%',
+  origin: 'left',
+  opacity: null
+});
+
+ScrollReveal().reveal('.mini-title', {
+  duration: 2000,
+  distance: '150%',
+  origin: 'left',
+  opacity: null
+});
+
+ScrollReveal().reveal('.words', {
+  duration: 2000,
+  distance: '150%',
+  origin: 'right',
+  opacity: null
+});
+
+ScrollReveal().reveal('.graph', {
+  duration: 2000,
+  distance: '150%',
+  origin: 'bottom',
+  opacity: null
+});
